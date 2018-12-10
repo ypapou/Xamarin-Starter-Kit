ea190ede2f14a7a7a6fa7250f2d31264854c6930
Yauhen Papou <yauhen.papou@gmail.com>
2018-12-10 23:34:45 +0300

    Add connectivity changed weak subscription method.

---
+++ Company.App.Infrastructure/Connectivity/ConnectivityWeakEventSubscriptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FlexiMvvm.Weak.Subscriptions;
+using Xamarin.Essentials;
+
+namespace Company.App.Infrastructure.Connectivity
+{
+    public static class ConnectivityWeakEventSubscriptions
+    {
+        public static IDisposable ConnectivityChangedWeakSubscribe(
+            this IConnectivity connectivity,
+            EventHandler<ConnectivityChangedEventArgs> connectivityChangedHandler)
+        {
+            return new WeakEventSubscription<IConnectivity, ConnectivityChangedEventArgs>(
+                connectivity,
+                (eventSource, eventHandler) => eventSource.ConnectivityChanged += eventHandler,
+                (eventSource, eventHandler) => eventSource.ConnectivityChanged -= eventHandler,
+                connectivityChangedHandler);
+        }
+    }
+}
