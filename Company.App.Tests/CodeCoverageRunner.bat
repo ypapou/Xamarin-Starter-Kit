b4ee0b3fef42b9bb0db59c2bca42c1edbc2be040
Aliaksei Safonau <Aliaksei_Safonau@epam.com>
2018-12-20 10:21:08 +0300

    Add CoverLet as code coverage analyzer

--- Company.App.Tests/CodeCoverageRunner.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath=%ThisScriptsDirectory%CalculateCodeCoverage.ps1
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'";
---
+++ Company.App.Tests/Scripts/GenerateCodeCoverageReport.bat
@@ -0,0 +1,1 @@
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%./build.ps1'";
