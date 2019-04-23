d5671046e7d64737ddc121773d474c60b791c982
Yauhen Papou <yauhen_papou@epam.com>
2019-04-23 15:35:37 +0300

    Add custom forward navigation strategy.

---
+++ Company.App.Droid/Navigation/ForwardNavigationStrategyExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Android.Content;
+using Android.OS;
+using FlexiMvvm.Navigation;
+
+namespace Company.App.Droid.Navigation
+{
+    public static class ForwardNavigationStrategyExtensions
+    {
+        public static ForwardNavigationDelegate StartActivity(
+            this ForwardNavigationStrategy navigationStrategy,
+            bool clearBackStack,
+            bool singleTop,
+            Bundle options = null)
+        {
+            return (sourceView, targetViewIntent, requestCode) =>
+            {
+                if (clearBackStack)
+                {
+                    targetViewIntent.AddFlags(ActivityFlags.ClearTask | ActivityFlags.ClearTop | ActivityFlags.NewTask);
+                }
+
+                if (singleTop)
+                {
+                    targetViewIntent.AddFlags(ActivityFlags.SingleTop);
+                }
+
+                sourceView.StartActivity(targetViewIntent, options);
+            };
+        }
+    }
+}
