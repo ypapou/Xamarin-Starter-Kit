769e3471cdcd3c42beb3d144fba349ae7b3d06c2
Yauhen Papou <yauhen.papou@gmail.com>
2019-08-20 00:11:14 +0300

    [Android] Update ForwardNavigationStrategyExtensions.

--- Company.App.Droid/Navigation/ForwardNavigationStrategyExtensions.cs
+++ Company.App.Droid/Navigation/ForwardNavigationStrategyExtensions.cs
@@ -1,30 +1,15 @@
 ﻿using Android.Content;
-using Android.OS;
 using FlexiMvvm.Navigation;
 
 namespace Company.App.Droid.Navigation
 {
     public static class ForwardNavigationStrategyExtensions
     {
-        public static ForwardNavigationDelegate StartActivity(
-            this ForwardNavigationStrategy navigationStrategy,
-            bool clearBackStack,
-            bool singleTop,
-            Bundle options = null)
+        public static ForwardNavigationDelegate ClearBackStack(this ForwardNavigationStrategy navigationStrategy)
         {
             return (sourceView, targetViewIntent, requestCode) =>
             {
-                if (clearBackStack)
-                {
-                    targetViewIntent.AddFlags(ActivityFlags.ClearTask | ActivityFlags.ClearTop | ActivityFlags.NewTask);
-                }
-
-                if (singleTop)
-                {
-                    targetViewIntent.AddFlags(ActivityFlags.SingleTop);
-                }
-
-                sourceView.StartActivity(targetViewIntent, options);
+                targetViewIntent.AddFlags(ActivityFlags.ClearTask | ActivityFlags.ClearTop | ActivityFlags.NewTask);
             };
         }
     }
